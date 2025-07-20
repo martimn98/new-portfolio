1999a66a682f8a611332e69b51f7abb70d6c475d
mnorte <martimn1998@gmail.com>
2025-07-20 12:33:21 +0100

    fix image path

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -1,9 +1,8 @@
 import NextImage, { ImageProps } from "next/image";
 
 export function Image({ ...imageProps }: ImageProps) {
-  const isProduction = process.env.NODE_ENV === "production";
   const basePath = process.env.PAGES_BASE_PATH ?? "";
-  const src = isProduction ? imageProps.src : basePath + imageProps.src;
+  const src = basePath + imageProps.src;
 
   return <NextImage {...imageProps} src={src} />;
 }
